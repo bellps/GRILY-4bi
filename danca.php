85f89ebca011f7791d7f0d3f014d638fc0cdd515
bellps <isabella020240@hotmail.com>
2018-11-12 22:54:09 -0200

    primeirão

---
+++ danca.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+        crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="css-js/css-aditional.css">
+    <link rel="stylesheet" href="css-js/Semantic-UI-CSS-master/semantic.min.css">
+    <script src="main.js"></script>
+</head>
+
+<body class="bg">
+    <nav class="navbar navbar-expand-md navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="">GRILY</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="sobre.php">Sobre nós</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            Cursos
+                        </a>
+                        <div class="dropdown-menu colorTree" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="teatro.php">Teatro</a>
+                            <a class="dropdown-item" href="danca.php">Dança</a>
+                            <a class="dropdown-item" href="pintura.php">Pintura</a>
+                            <a class="dropdown-item" href="musica.php">Música</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="midia.php">Mídia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="form.php">Fale conosco</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="banner">
+        <img src="midia/predio-frente2.jpg" class="banner-image">
+        <p class="text-light display-4 centrodois">insira título aqui</p>
+    </div>
+  
+    <footer class="colorTwo">
+        <div class="ui center aligned container">
+            <br>
+            <h1>GRILY</h1>
+            <i class="facebook icon"></i>
+            <i class="twitter icon"></i>
+            <i class="instagram icon"></i>
+            <i class="fort awesome icon"> </i>
+            <div class="ui divider"></div>
+            <p>Escola de arte GRILY é uma escola de artes, fundada em 2000 por Marco Nanini, que já está a mais de 40
+                anos no mercado, formando verdadeiros profissionais na arte.
+                <br><br><strong>© Todos os direitos reservados. Fundação GRILY</strong></p>
+            <br>
+        </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
+        crossorigin="anonymous"></script>
+    <script src="css-js/js-aditional.js"></script>
+    <script src="css-js/Semantic-UI-CSS-master/semantic.min.js"></script>
+    <script>
+        $('.special .image').dimmer({
+            on: 'hover'
+        });
+    </script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+        crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="css-js/css-aditional.css">
+    <link rel="stylesheet" href="css-js/Semantic-UI-CSS-master/semantic.min.css">
+    <script src="main.js"></script>
+</head>
+
+<body class="bg">
+    <nav class="navbar navbar-expand-md navbar-light fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="">GRILY</a>
+            <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="sobre.php">Sobre nós</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            Cursos
+                        </a>
+                        <div class="dropdown-menu colorTree" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="teatro.php">Teatro</a>
+                            <a class="dropdown-item" href="danca.php">Dança</a>
+                            <a class="dropdown-item" href="pintura.php">Pintura</a>
+                            <a class="dropdown-item" href="musica.php">Música</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="midia.php">Mídia</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link js-scroll-trigger" href="form.php">Fale conosco</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="banner">
+        <img src="midia/predio-frente.jpg" class="banner-image">
+        <div class="centro">
+            <h1 class="display-1 text-light">GRILY</h1>
+            <p class="lead text-light">A escola de artes mais renomada do Brasil</p>
+        </div>
+    </div>
+    <div class="colorOne">
+        <br>
+        <br>
+        <div class="ui justified container">
+            <h1 class="ui huge orange header">Sobre nós...</h1>
+            <div class="ui divider"></div>
+            <div class="row">
+                <div class="col-md-6">
+                    <p>Na GRILY são formados profissionais de ótimo nível, capacitados para enfrentar com sucesso as
+                        exigências
+                        do mercado de trabalho. O método de nossa escola é único e garante o desenvolvimento artístico
+                        e
+                        técnico de seus alunos. O ambiente em todas as unidades é adequado e agradável para o ensino.
+                        Você
+                        recebe atendimento personalizado dentro do seu ritmo, ou seja, as turmas são formadas com o
+                        número
+                        máximo de dezoito alunos onde o professor(es) atende às necessidades de cada um
+                        individualmente,
+                        obtendo assim, um excelente aproveitamento.
+                    </p>
+                    <button class="ui inverted orange button">Ler mais</button>
+                </div>
+                <div class="col-md-6 mx-auto">
+                    <div class="ui slide masked reveal image">
+                        <img src="https://images4.alphacoders.com/287/287326.jpg" class="visible content">
+                        <img src="http://image.al.com/home/bama-media/width600/img/news_birmingham_impact/photo/gardendale-high-schooljpg-a90b5be907fd546b.jpg"
+                            width="110%" class="hidden content">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <br><br>
+    </div>
+    <div>
+        <br>
+        <br>
+        <br>
+        <div class="ui center aligned container">
+            <h1>Confira imagens</h1>
+            <div class="ui medium images">
+                <img src="https://jaddisonschool.com/wp-content/uploads/J-Addison-Private-School-Markham-Ontario-Canada-science-lab-1.jpg">
+                <img src="https://www.concordmonitor.com/getattachment/110da633-c73f-4734-885e-2dce58cf7c2a/RMSspill-cm-092016-ph01">
+                <img src="http://assets.ecenglish.com/blogs/uploads/sites/34/2014/10/see-inside-our-school.jpg">
+                <img src="https://www.eveningexpress.co.uk/wp-content/uploads/sites/10/2018/07/5b52fb8a69241-559x372.jpg">
+                <img src="https://jaddisonschool.com/wp-content/uploads/J-Addison-Private-School-Markham-Ontario-Canada-Cafe-J-Food-Services-1.jpg"
+                    alt="">
+                <img src="http://assets.inarkansas.com/64900/north-little-rock-high-school-renovations-763.jpg" alt="">
+                <img src="https://news-img-9a9cfd28311db221b19757424a2f3b66.s3.amazonaws.com/articles/MillCreekERW11457393003.jpg"
+                    alt="">
+                <img src="https://merchantview360.com/wp-content/uploads/2014/05/IMG_18801.jpg" alt="">
+                <img src="https://cdn.vox-cdn.com/thumbor/l3okHkkHR0iaqGtnbECgeToVAog=/0x0:800x533/920x613/filters:focal(336x202:464x330):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/61311607/JonesColl9.0.0.1485190576.0.jpg"
+                    alt="">
+            </div>
+        </div>
+        <br>
+        <br>
+        <br>
+    </div>
+    <div class="colorFour">
+        <br>
+        <br>
+        <br>
+        <div class="ui container">
+            <div class="ui grid">
+                <div class="ten wide column">
+                    <div class="ui center aligned container">
+                        <div class="ui special cards">
+                            <div class="card">
+                                <div class="blurring dimmable image">
+                                    <div class="ui dimmer">
+                                        <div class="content">
+                                            <div class="center">
+                                                <a href="https://semantic-ui.com/views/card.html">
+                                                    <h1>Teatro</h1>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="https://escolarecriarte.com.br/wp-content/themes/recriarte-theme/assets/midia/template-imagem-destacada-teatro-adulto.jpg">
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="blurring dimmable image">
+                                    <div class="ui dimmer">
+                                        <div class="content">
+                                            <div class="center">
+                                                <a href="https://semantic-ui.com/views/card.html">
+                                                    <h1>Dança</h1>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="http://dizzyfeetfoundation.org/wp-content/uploads/2015/06/2015-DFF-Gala_2015-DFF-Gala_567135339AW213_5th_Annual_C-800x800.jpg">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ui special cards">
+                            <div class="card">
+                                <div class="blurring dimmable image">
+                                    <div class="ui dimmer">
+                                        <div class="content">
+                                            <div class="center">
+                                                <a href="https://semantic-ui.com/views/card.html">
+                                                    <h1>Pintura</h1>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="https://format-com-cld-res.cloudinary.com/image/private/s--VHMFroAF--/c_crop,h_741,w_1097,x_0,y_153/c_fill,g_center,h_800,w_800/a_auto,fl_keep_iptc.progressive,q_95/v1/cebcb5751b4fa5320ad41cf004a23e87/Annalisa_Schaefer_profile_pic.jpg">
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="blurring dimmable image">
+                                    <div class="ui dimmer">
+                                        <div class="content">
+                                            <div class="center">
+                                                <a href="https://semantic-ui.com/views/card.html">
+                                                    <h1>Música</h1>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <img src="https://slimspresents.com/wp-content/uploads/2018/02/A.GRACE_.0214-800x800.jpg">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="center aligned six wide column">
+                    <br><br><br><br><br><br><br><br><br><br><br><br><br>
+                    <h1 class="right aligned"><i class="hand point left outline icon"></i><br>Conheça nossos cursos</h1>
+                    <p>Na GRILY temos apenas os melhores cursos artísticos de toda América Latina. Clique sobre uma das
+                        iamgens e confira detalhes!</p>
+                </div>
+
+            </div>
+            <br>
+            <br>
+            <br>
+        </div>
+    </div>
+
+
+
+    <footer class="colorTwo">
+        <div class="ui center aligned container">
+            <br>
+            <h1>GRILY</h1>
+            <i class="facebook icon"></i>
+            <i class="twitter icon"></i>
+            <i class="instagram icon"></i>
+            <i class="fort awesome icon"> </i>
+            <div class="ui divider"></div>
+            <p>Escola de arte GRILY é uma escola de artes, fundada em 2000 por Marco Nanini, que já está a mais de 40
+                anos no mercado, formando verdadeiros profissionais na arte.
+                <br><br><strong>© Todos os direitos reservados. Fundação GRILY</strong></p>
+            <br>
+        </div>
+    </footer>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em"
+        crossorigin="anonymous"></script>
+    <script src="css-js/js-aditional.js"></script>
+    <script src="css-js/Semantic-UI-CSS-master/semantic.min.js"></script>
+    <script>
+        $('.special .image').dimmer({
+            on: 'hover'
+        });
+    </script>
+</body>
+
+</html>
